7bbd0c26e6c45f89e044e516dd46026369035da9
zauber (Henrik Zauber) <henrik.zauber@mdc-berlin.de>
2013-11-29 09:13:10 +0000

    buggy
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mqqc@92 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/LoadSettings.R
@@ -0,0 +1,10 @@
+LoadSettings <- 
+function(...){
+	for(i in 1:length(list(...))){
+		assign(names(list(...)[i]),list(...)[[i]],envir = .GlobalEnv)
+		
+		
+	}
+}
+
+#bla(folder = NULL,MQ = NULL,fastaFile = NULL,fun= mqStarter,temp.name = "test", DeleteFiles = F,cores = NULL,SpeciesTable = T,templateFasta = "._.*_.*_PLACEHOLDER_",placeholder = "PLACEHOLDER",FUNLAST = FUNFINAL,sucFolder = "_RmqqcFile_Processed",htmloutPath = "D:/_RmqqcFile_mqqcHtml",gui = T,SendMail = T, automatedStart = F)
