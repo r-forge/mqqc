337909ff65eb41a6a56a3b3254cbce645a83d9f9
zauber (Henrik Zauber) <henrik.zauber@mdc-berlin.de>
2013-12-02 15:12:34 +0000

    Added
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mqqc@97 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/PrepareMail.R
@@ -0,0 +1,38 @@
+
+PrepareMail <- 
+function(Title= "Tomato",Message="Merry Christmas",recipient = "santa\\@clause.np"){
+
+MailList <- list()
+MailList$user <- "user"
+MailList$pass <- "pass"
+MailList$server <- "server"
+MailList$to <- recipient
+MailList$from <- "from"
+MailList$title <- Title
+MailList$message <- Message
+
+MailSettings <- list.files(paste(path.package("mqqc"),"data",sep = "/"),pattern = "MailSettings",full.name = T)
+
+if(length(MailSettings) > 0){	
+		#print("hu")
+
+	MailSettings <- readLines(MailSettings)
+		#print("hu")
+
+	if(length(MailSettings) ==4){
+		MailList[c(1,2,3,5)] <- MailSettings	
+	}
+	MailList <<- MailList
+	perl.init <- list.files(paste(path.package("mqqc"),"data",sep = "/"),pattern = "mailPerl.pl",full.name = T)
+
+	MailList <<- MailList
+	perl.init <<- perl.init
+	MailOverPerl(MailList,perl.init)
+		
+	}
+
+
+
+}
+
+
